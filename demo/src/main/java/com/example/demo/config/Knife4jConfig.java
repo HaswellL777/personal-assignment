6417e0011c2be5a636113c5daeee3da7e311b465
Nick <16323927+nicknack503@user.noreply.gitee.com>
2025-11-03 15:29:49 +0800

    backup: 备份当前工作代码（同步前）

---
+++ demo/src/main/java/com/example/demo/config/Knife4jConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Knife4j 配置类
+ * API文档访问地址: http://localhost:8080/usermanagement/doc.html
+ */
+@Configuration
+public class Knife4jConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("用户管理系统 API 文档")
+                        .description("基于 Spring Boot 3 + MyBatis 的用户管理系统 RESTful API")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("开发团队")
+                                .email("dev@example.com")
+                                .url("https://example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
